0a59d7c0a5c22fb4239a93749d6f9b68d55d0f23
ishitasamadhiya <146588235+ishitasamadhiya@users.noreply.github.com>
2024-04-25 08:13:37 -0700

    initial

---
+++ Glitch/Glitch/GlitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlitchApp.swift
+//  Glitch
+//
+//  Created by StudentAM on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GlitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
